75d99e2b8b4fa34d4cee3531f9a5ec39eea6750e
Ephraim69 <ephraimigbinosa@gmail.com>
2022-06-30 20:54:44 +0000

    a C program that prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+* main - the main function
+* Return: returns 1
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
