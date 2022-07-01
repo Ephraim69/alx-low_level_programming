1f6b6d3cfdc739678400b52e1097662ae13f5cf6
Ephraim69 <ephraimigbinosa@gmail.com>
2022-07-01 21:11:46 +0000

    a C program that prints exactly and that piece of art is useful\" - Dora Korpar, 2015-10-19

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
 * main - the main function
@@ -6,8 +7,6 @@
 */
 int main(void)
 {
-	char qt[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	fprintf(stderr, "%s\n", qt);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
