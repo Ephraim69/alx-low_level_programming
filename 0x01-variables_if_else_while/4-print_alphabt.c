affc486198770dc889f11837d58e6e2cb4ea9fc1
Ephraim69 <ephraimigbinosa@gmail.com>
2022-07-01 09:33:42 +0000

    a program that prints the alphabet except the letter q and 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+* main - prints all leters of the alphabet except 'e' amd 'q'
+* Return: retuens nothing
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	putchar('\n');
+}
