ba85f6282481e085f695d000c73e09d1909ae437
Ephraim69 <ephraimigbinosa@gmail.com>
2022-07-01 11:34:04 +0000

    c program on variables, conditionals and loops

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
-* main - two nested loops that print the entire alphabet in just lower and upper case
+* main - two loops that print the alphabet lower and upper case
 * Return: returns 0
 */
 int main(void)
