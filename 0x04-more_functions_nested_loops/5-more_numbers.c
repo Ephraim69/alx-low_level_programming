4c0ec3d54ce085a3bb622baf17cf24c11b463710
Ephraim69 <ephraimigbinosa@gmail.com>
2022-07-07 13:21:27 +0000

    prints numbers 0 to 9, ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* more_numbers - prints numbers 0 to 9, ten times
+* Return: returns nothing
+*/
+void more_numbers(vod)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar(j/10 + '0');
+			_putchar(j%10 + '0');
+		}
+		_putchar('\n');
+	}
+}
