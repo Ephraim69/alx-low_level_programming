5919866e986a4665f16649a4866508e9a6fd4a9d
Ephraim69 <ephraimigbinosa@gmail.com>
2022-07-07 14:48:47 +0000

    the FizzBuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - a program to solve the 'FizzBuzz' test
+* Return: returns 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
