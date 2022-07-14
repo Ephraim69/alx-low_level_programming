f9cf578ede04809f6c5e248ef6532a1ce8e00a5d
Ephraim69 <ephraimigbinosa@gmail.com>
2022-07-14 16:20:49 +0000

    a fuction that capitalizes the first letter of each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* cap_string - capitalizes the first letter of eacg word
+* @s: a pointer to the string
+* Return: returns s
+*/
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i + 1] >= 97 && s[i + 1] <= 122)
+		{
+			if (s[i] == ' ' || s[i] == '\n' || s[i] == ','
+				|| s[i] == ';' || s[i] == '.' || s[i] == '!'
+					|| s[i] == '?' || s[i] == '"' || s[i] == '('
+						|| s[i] == ')' || s[i] == '{' || s[i] == '}')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+			if (s[i] == '\t')
+			{
+				s[i] = ' ';
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
