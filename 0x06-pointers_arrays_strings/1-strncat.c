c44b8ed2cf307385f0694d964960932b9f289bdc
Ephraim69 <ephraimigbinosa@gmail.com>
2022-07-14 13:23:39 +0000

    a fuction that cocantenates two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strncat - a function that cocantenates two strings
+* @dest: the pointer to the cocantenated string
+* @src: the string to be appended
+* @n: the number of bytes from src to be appended
+* Return: returns a pointer to the string dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, c = 0, d = 0, j = 0;
+
+	for (i = 0; dest[i] != 0; i++)
+		c++;
+	for (i = 0; src[i] != 0; i++)
+		d++;
+	for (i = c; i < c + n; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	return (dest);
+}
