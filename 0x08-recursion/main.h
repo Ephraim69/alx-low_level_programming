860dc84a2d3ff297c4fe10e0265d37f70337ff7c
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-07-20 14:43:54 +0000

    added header file main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,12 +1,13 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
-}
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif /*MAIN_H*/
