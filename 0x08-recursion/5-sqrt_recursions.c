bbb0a29da0c60809531acc5ce13450281d59ca9e
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-07-21 00:30:39 +0000

    a function that finds the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursions.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int sqrt_function(int n, int i);
+
+/**
+* _sqrt_recursion - a function that returns the square root of a number
+* @n: the number to be operated on
+* Return: returns the square root of n as an int
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_function(n, 0));
+}
+
+/**
+ * sqrt_function - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int sqrt_function(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqrt_function(n, i + 1));
+}
