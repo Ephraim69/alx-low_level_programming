417165d7db9c634b9724a73500209ad95742f245
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-07-26 09:34:38 +0000

    a function that concantenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 
+int count_str1(char *s1);
+int count_str2(char *s2);
+
 /**
 * str_concat  - a function that concantenates two strings
 * @s1: the destination string
@@ -12,34 +15,72 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i;
-	int j = 0;
-	int c = 0;
-	int c1 = 0;
-	int c2 = 0;
+	int j = 0, c = 0, c1 = 0, c2 = 0;
 	char *newStr;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		c1++;
-	}
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-		c2++;
-	}
+	c1 = count_str1(s1);
+	c2 = count_str2(s2);
+
 	c = c1 + c2 + 1;
 	newStr = (char *) malloc(c * sizeof(char));
 	if (newStr == NULL)
 		return (NULL);
-
-	for (i = 0; i < c1; i++)
+	i = 0;
+	if (s1 != NULL)
 	{
-		newStr[i] = s1[i];
+		for (i = 0; i < c1; i++)
+		{
+			newStr[i] = s1[i];
+		}
 	}
-	for ( ; i < c; i++)
+	if (s2 != NULL)
 	{
-		newStr[i] = s2[j];
-		j++;
+		for ( ; i < c; i++)
+		{
+			newStr[i] = s2[j];
+			j++;
+		}
 	}
 	newStr[c - 1] = '\0';
 	return (newStr);
+}
+
+/**
+* count_str1 - counts the number of characters in s1
+* @s1: the string to be counted
+* Return: returns the number of characters an an int, returns 0 if s1 is NULL
+*/
+int count_str1(char *s1)
+{
+	int i;
+	int c1 = 0;
+
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			c1++;
+		}
+	}
+	return (c1);
+}
+
+/**
+* count_str2 - counts the number of characters in s2
+* @s2: the string to be counted
+* Return: returns the number of characters an an int, returns 0 if s2 is NULL
+*/
+int count_str2(char *s2)
+{
+	int i;
+	int c2 = 0;
+
+	if (s2 != NULL)
+	{
+		for (i = 0; s2[i] != '\0'; i++)
+		{
+			c2++;
+		}
+	}
+	return (c2);
 }
