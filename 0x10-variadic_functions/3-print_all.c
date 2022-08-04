7ba7f5ba9ad84e583adef3d13e2d7be036e9642e
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-08-04 17:44:42 +0000

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,44 +7,43 @@
 */
 void print_all(const char * const format, ...)
 {
-	int i = 0;
+	int i = 0, j = 0;
 	va_list args;
 	char *ptr;
 
 	va_start(args, format);
-	while (format[i] != '\0')
-	{
-		switch (format[i])
+	while (j < 1 && format != NULL)
+		while (format[i] != '\0')
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				ptr = va_arg(args, char *);
-				if (ptr == NULL)
-				{
-					printf("(nil)");
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
 					break;
-				}
-				printf("%s", ptr);
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					ptr = va_arg(args, char *);
+					if (ptr == NULL)
+						ptr = "(nil)";
+					printf("%s", ptr);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			if (format[i + 1] == '\0')
+			{
 				break;
-			default:
-				i++;
-				continue;
+			}
+			printf(", ");
+			i++;
+			j++;
 		}
-		if (format[i + 1] == '\0')
-		{
-			printf("\n");
-			break;
-		}
-		printf(", ");
-		i++;
-	}
+	printf("\n");
 	va_end(args);
 }
