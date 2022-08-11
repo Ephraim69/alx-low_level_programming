ed501c68e01e1201aec4672cb6380ba0a935ffe0
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-08-11 22:35:20 +0000

    a function that prints data from a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* print_list - a function that prints all elements of a singly-linked list
+* @h: a pointer to a list_t list
+* Return: returns the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (1);
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
