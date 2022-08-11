452f612bba195c46cc8d2413157f220c8bdc9bed
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-08-11 23:14:21 +0000

    singly-linked_lists

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
