2ba2521e3981765505949ab8e48932ff2f659eec
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-08-16 13:19:19 +0000

    added check for if malloc fails

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *tmp;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 	tmp = *head;
 
 	if (*head == NULL)
