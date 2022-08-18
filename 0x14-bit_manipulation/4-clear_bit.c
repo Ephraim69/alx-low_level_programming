14c2c537117c4fadcb691bc7ac24cd495515d136
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-08-18 23:52:31 +0000

    a function that clears the value of a bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* clear_bit - a function that clears the value of a bit at a given index
+* @n: the number to be operated on
+* @index: the index
+* Return: returns 1 (Success), -1 (error)
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	int mask;
+
+	if (index > 63)
+		return (-1);
+
+	mask = 1 << index;
+
+	*n = (*n & ~mask);
+
+	return (1);
+}
