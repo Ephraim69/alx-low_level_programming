998e648bde41c579f8324a70718e4699fe825fcb
Ephraim <ephraimigbinosa@gmail.com>
2022-09-29 16:42:01 +0100

    fix edge case

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,6 +17,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
+
+	if (head == NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		head = &new;
+		return (new);
+	}
+
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
