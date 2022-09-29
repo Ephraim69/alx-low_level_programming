8a928f2dc05bc5d8eec7584f232711ed7c7b3b09
Ephraim <ephraimigbinosa@gmail.com>
2022-09-29 16:48:32 +0100

    add check for NULL argument

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,20 +12,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL;
 
+	if (!head)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 
 	new->n = n;
-
-	if (head == NULL)
-	{
-		new->next = NULL;
-		new->prev = NULL;
-		head = &new;
-		return (NULL);
-	}
-
 	new->next = *head;
 	new->prev = NULL;
 	*head = new;
