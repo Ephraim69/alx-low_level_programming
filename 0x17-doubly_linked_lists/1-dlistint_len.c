db24d4f32987c96861e770fb48bfc9ce41509768
Ephraim <ephraimigbinosa@gmail.com>
2022-09-29 16:19:35 +0100

    add function to count dllist

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - a function that counts the numebr of elements in a
+ * dlistint list
+ * @h: the head of the linked list
+ *
+ * Return: returns the number of elements
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *tmp = h;
+	size_t i = 0;
+
+	while (tmp)
+	{
+		i++;
+	}
+
+	return (i);
+}
