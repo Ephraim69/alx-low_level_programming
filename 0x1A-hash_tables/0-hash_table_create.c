cf10aa5885bd1bddc791ec04708fae92a16bc7c9
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-10-21 07:44:34 +0000

    make changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,5 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new_table)
 		return (NULL);
 
+	new_table->size = size;
+	new_table->array = calloc(size, sizeof(hash_node_t *));
+	if (new_table->array == NULL)
+		return (NULL);
+
 	return (new_table);
 }
