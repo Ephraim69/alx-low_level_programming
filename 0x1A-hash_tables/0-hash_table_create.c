27fda41e3b3672bc18abca5488b3edd0844373fd
Ephraim69 <Ephraimigbinosa@gmail.com>
2022-10-21 07:47:50 +0000

    make changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	new_table = malloc(sizeof(hash_node_t) * size);
+	new_table = malloc(sizeof(hash_table_t);
 	if (!new_table)
 		return (NULL);
 
